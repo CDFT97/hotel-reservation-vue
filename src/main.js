d83afd756021a794f027b3c20196530331cf5138
Cesar Febres <febrescesar7@gmail.com>
2023-11-28 15:50:25 -0400

    axios default url

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios';
 import { createPinia } from 'pinia'
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
@@ -9,6 +10,8 @@ import './assets/css/app.css'
 
 // import router
 import router from './router'
+
+axios.defaults.baseURL = process.env.VUE_APP_BACKEND_API
 const pinia = createPinia()
 const app = createApp(App)
 app.use(router)
