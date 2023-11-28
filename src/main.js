f96a9f1400b0a8abdc85589978e5b022d1eac278
Cesar Febres <febrescesar7@gmail.com>
2023-11-27 20:36:54 -0400

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createPinia } from 'pinia'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
-createApp(App).mount('#app')
+// import css
+import './assets/css/app.css'
+
+// import router
+import router from './router'
+const pinia = createPinia()
+const app = createApp(App)
+app.use(router)
+app.use(pinia)
+app.use(VueSweetalert2);
+app.mount('#app')
